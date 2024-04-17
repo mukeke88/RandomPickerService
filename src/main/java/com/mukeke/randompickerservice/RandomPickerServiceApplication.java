cc4dc506d9b41cefc746a4f886285926aa348de8
mukeke88 <makaikai0628@gmail.com>
2024-04-17 10:39:50 +0800

    Initial commit

---
+++ src/main/java/com/mukeke/randompickerservice/RandomPickerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mukeke.randompickerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPickerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomPickerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mukeke/randompickerservice/RandomPickerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukeke.randompickerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPickerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
